2eb6c675fbc3191f35628625a4bf45820d16b3d3
desy <desypuspita685@gmail.com>
2026-05-05 18:36:39 +0700

    praktikum9

---
+++ 09 Kamera/kamera_flutter/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'widget/takepicture_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Ambil daftar kamera
+  final cameras = await availableCameras();
+
+  // Ambil kamera pertama
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
